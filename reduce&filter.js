95276a8f309f31c57a92aef087bee33264a853ae
MohammadJozaa7 <m.jozaa@yahoo.com>
2019-07-18 12:20:03 +0300

    upload till 17 advanced part

--- reduce&filter.js
+++ reduce&filter.js
@@ -1,4 +1,4 @@
-console.log('Here is : ', 'Reduce & Filter') \
+console.log('Here is : ', 'Reduce & Filter')
 
 //Using Reduce 1 -5
 var persons = [
@@ -9,10 +9,9 @@ var persons = [
   { name: { first: 'Soso', last: 'Al-Amora' }, age: 67 }
 ];
 
-
-
 /*
 1
+Using Reduce
 Using the varabile persons
 Create a function called avgAge
 that accept an array
@@ -24,6 +23,7 @@ Ex: avgAge(persons) => 41.2
 
 /*
 2
+Using Reduce
 Using the varabile persons
 Create a function called longestName
 that accept an array
@@ -35,6 +35,7 @@ Ex: longestName(persons) => 'Soso Al-Amora'
 
 /*
 3
+Using Reduce
 Create a function called maxNumber
 that accept an array
 and return max number
@@ -45,6 +46,7 @@ Ex: maxNumber([1,2,4,9]) => 9
 
 /*
 4
+Using Reduce
 Create a function called repeatChar
 that accept a string and char
 and return number times that this char repeat inside the string
@@ -55,6 +57,7 @@ Ex: repeatChar("hello world",w) => 1
 
 /*
 5
+Using Reduce
 Create a function called usAndNumberBeetweenUs
 that accept two numbers
 and return array of these two numbers and the numbers between them
@@ -63,14 +66,10 @@ Ex: usAndNumberBeetweenUs(2,5) => [2,3,4,5]
 */
 
 
-
-
-
-
-
 //Filter 6 - 11
 /*
 6
+Using Filter
 Create a function called evenOnly
 that accept an array
 and return an array of even number only
@@ -81,6 +80,7 @@ Ex: evenOnly([1,8,6,4]) => [8,6,4]
 
 /*
 7
+Using Filter
 Create a function called multiFour
 that accept an array
 and return an array of these number that is a mutiply by 4
@@ -91,6 +91,7 @@ Ex: multiFour([1,8,6,4]) => [8,4]
 
 /*
 8
+Using Filter
 **this question not that easy mybe you will need to use two function inside each other
 
 Create a function called containChar
@@ -104,6 +105,7 @@ Ex: containChar(["hello","world"],l) => ["hello","world"]
 
 /*
 9
+Using Filter
 Create a function called evenIndexOddLength
 that accept an array of strings
 and return an array that have the string with odd length in even index
@@ -115,6 +117,7 @@ Ex: evenIndexOddLength(strings) => ["madrasa"]
 
 /*
 10
+Using Filter
 Using the varabile persons
 Create a function called olderThan
 that accept an array and number
@@ -125,6 +128,7 @@ Ex: olderThan(persons,56) => [{ name: { first: 'Soso', last: 'Al-Amora' }, age: 
 
 /*
 11
+Using Filter
 Create a function called shorterThan
 that accept an array of strings and a number
 and return the shorter string than the number
@@ -133,5 +137,89 @@ var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
 Ex: shorterThan(strings,5) => ["alex","emad","hala"]
 */
 
+/*
+Advanced Part {for your benefits}
+
+Dont solve any question here if you didnt finish all the question a bove with your pair
+
+This part you can try it
+You should solve this part solo not with your pair
+
+This part is extra (and there is no help from the trainer) it is good to try it
+*/
+
+//Using filter 12 -15
+
+/*
+12
+Using Filter
+Create a function called longerThan
+that accept an array of strings
+and return the longer string than the number
+
+var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
+Ex: longerThan(strings,4) => ["mercer","madrasa","rashed2"]
+*/
+
+/*
+13
+Using Filter
+Create a function called onlyOneWord
+that accept an array of strings
+and return only those strings with a single word (no spaces)
+
+var strings= [ 'return', 'phrases', 'with one word' ];
+Ex: onlyOneWord(strings) => [ 'return', 'phrases' ]
+*/
+
+
+
+/*
+14
+Using Filter
+Create a function called positiveRowsOnly
+that accept an array of array of numbers(matrix)
+and return only the rows in the matrix that have all positive integers
+
+var numbers= [[ 1, 10, -100 ], [ 2, -20, 200 ],[ 3, 30,  300 ]];
+Ex: positiveRowsOnly(numbers) => [ 3, 30,  300 ]
+*/
+
+
+/*
+15
+Using Filter
+Create a function called allSameVowels
+that accept an array of strings
+return only those words where all the vowels are the same
+
+var strings= [ 'racecar', 'amalgam', 'oligopoly', 'zoom' ];
+Ex: allSameVowels(strings) =>  [ 'amalgam', 'zoom' ]
+*/
+
+//Using Reduce 16 -
+
+/*
+16
+Using Reduce
+Create a function called objectify
+that accept an array of pairs array
+and turns an array of arrays into an object
+
+var array= [[ 'Thundercats', '80s' ],[ 'The Powerpuff Girls', '90s' ],[ 'Sealab 2021', '00s' ]]
+Ex: objectify(array) =>  { 'Thundercats': '80s', 'The Powerpuff Girls': '90s', 'Sealab 2021': '00s' };
+*/
+
+
+/*
+17
+Using Reduce
+Create a function called luckyNumbers
+that accept an array of number
+and turns an array of arrays into an object
+
+var array= [ 30, 48, 11, 5, 32 ]
+Ex: luckyNumbers(array) => 'Your lucky numbers are: 30, 48, 11, 5, and 32';
+*/
 
 // if you finish the exercises review the material of the week and help your classmate
